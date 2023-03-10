5afb9822a9d71052793c19ae0ae088c5cab714e0
easygithdev <easygithdev@gmail.com>
2023-03-10 11:35:03 +0100

    improve audio

--- src/Speech2text/Audio.php
+++ src/Speech2text/Audio.php
@@ -25,42 +25,30 @@ class Audio
         $this->headers = $headers;
     }
 
-
-    /*
-    The audio file to transcribe, in one of these formats: mp3, mp4, mpeg, mpga, m4a, wav, or webm.
-
-    model
-    string
-    Required
-    ID of the model to use. Only whisper-1 is currently available.
-    
-    prompt
-    string
-    Optional
-    An optional text to guide the model's style or continue a previous audio segment. The prompt should match the audio language.
-    
-    response_format
-    string
-    Optional
-    Defaults to json
-    The format of the transcript output, in one of these options: json, text, srt, verbose_json, or vtt.
-    
-    temperature
-    number
-    Optional
-    Defaults to 0
-    The sampling temperature, between 0 and 1. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic. If set to 0, the model will use log probability to automatically increase the temperature until certain thresholds are hit.
-    
-    language
-    string
-    Optional
-    The language of the input audio. Supplying the input language in ISO-639-1 format will improve accuracy and latency.
-*/
-
-    function transcription(string $audioFile, Model $model, string $prompt = '', ResponseFormat $responseFormat = ResponseFormat::JSON, float $temperature = 0, string $language = ''): string
+    protected function extensionAvailable(string $filename): bool
     {
+        $ext = pathinfo($filename, PATHINFO_EXTENSION);
+        return in_array($ext, ['mp3', 'mp4', 'mpeg', 'mpga', 'm4a', 'wav', 'webm']);
+    }
+
+    public function transcription(
+        string $audioFile,
+        Model $model = Model::WHISPER_1,
+        string $prompt = '',
+        ResponseFormat $responseFormat = ResponseFormat::JSON,
+        float $temperature = 0,
+        Language $language = Language::ENGLISH
+    ): string {
         if (!file_exists($audioFile)) {
             throw new Exception("Unable to locate file: $audioFile");
+        }
+
+        if (!$this->extensionAvailable($audioFile)) {
+            throw new Exception("Use one of these formats: mp3, mp4, mpeg, mpga, m4a, wav, or webm");
+        }
+
+        if ($model != Model::WHISPER_1) {
+            throw new \Exception("Only whisper-1 is currently available.");
         }
 
         if ($temperature < 0 or $temperature > 1) {
@@ -76,11 +64,59 @@ class Audio
         ];
 
         if (!empty($language)) {
-            $payload["language"] = $language;
+            $payload["language"] = $language->value;
         }
 
         $response =  $this->curl
             ->setUrl($this->apiUrl . self::END_POINT . '/transcriptions')
+            ->setHeaders(
+                $this->headers
+            )
+            ->setPayload($payload)
+            ->exec();
+
+        $this->curl->close();
+
+        return $response;
+    }
+
+    public function translation(
+        string $audioFile,
+        Model $model = Model::WHISPER_1,
+        string $prompt = '',
+        ResponseFormat $responseFormat = ResponseFormat::JSON,
+        float $temperature = 0
+    ): string {
+        if (!file_exists($audioFile)) {
+            throw new Exception("Unable to locate file: $audioFile");
+        }
+
+        if (!$this->extensionAvailable($audioFile)) {
+            throw new Exception("Use one of these formats: mp3, mp4, mpeg, mpga, m4a, wav, or webm");
+        }
+
+        if ($model != Model::WHISPER_1) {
+            throw new \Exception("Only whisper-1 is currently available.");
+        }
+
+        if ($temperature < 0 or $temperature > 1) {
+            throw new \Exception("Temperature to use, between 0 and 1");
+        }
+
+        $payload = [
+            "file" => curl_file_create($audioFile),
+            "model" => $model->value,
+            "prompt" => $prompt,
+            "response_format" => $responseFormat->value,
+            "temperature" => $temperature,
+        ];
+
+        if (!empty($language)) {
+            $payload["language"] = $language->value;
+        }
+
+        $response =  $this->curl
+            ->setUrl($this->apiUrl . self::END_POINT . '/translations')
             ->setHeaders(
                 $this->headers
             )
