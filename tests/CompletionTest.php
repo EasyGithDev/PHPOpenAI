5dff71fd489cee43dde3228b9bd962a1a5cf14f2
easygithdev <easygithdev@gmail.com>
2023-04-01 13:41:12 +0200

    adding stream test

--- tests/CompletionTest.php
+++ tests/CompletionTest.php
@@ -4,21 +4,47 @@ namespace EasyGithDev\PHPOpenAI;
 
 use EasyGithDev\PHPOpenAI\Helpers\ModelEnum;
 use PHPUnit\Framework\TestCase;
+use stdClass;
 
 final class CompletionTest extends TestCase
 {
 
-
-
     public function testCreate()
     {
 
-        $response = (new OpenAIClient(getenv('OPENAI_API_KEY')))->Completion()->create(
-            "text-davinci-003",
-            "Say this is a test",
-            user: 'phpunit'
-        )->getResponse();
+        $response = (new OpenAIClient(getenv('OPENAI_API_KEY')))
+            ->Completion()
+            ->create(
+                "text-davinci-003",
+                "Say this is a test",
+                user: 'phpunit'
+            )->getResponse();
 
         $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testStream()
+    {
+        $str = '';
+
+        $response = (new OpenAIClient(getenv('OPENAI_API_KEY')))
+            ->Completion()
+            ->setCallback(function ($ch, $data) use (&$str) {
+                $jsonData = json_decode(str_replace('data: ', '', trim(trim($data), '"')));
+                if ($jsonData && is_a($jsonData, stdClass::class)) {
+                    $str .= $jsonData?->choices[0]->text;
+                }
+                return mb_strlen($data);
+            })
+            ->create(
+                "text-davinci-003",
+                "Say this is a test",
+                stream: true,
+                echo: true,
+                user: 'phpunit'
+            )->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertTrue(str_contains($str, 'Say this is a test'));
     }
 }
